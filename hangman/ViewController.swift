74672fc0df9109194d23686ee02cfa49ff96dcad
Kasper Peulen <kasperpeulen@gmail.com>
2015-11-30 13:56:53 +0100

    Initial Commit

---
+++ hangman/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hangman
+//
+//  Created by Kasper Peulen on 11/30/15.
+//  Copyright © 2015 Kasper Peulen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
